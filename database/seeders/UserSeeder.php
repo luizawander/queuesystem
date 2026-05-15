d50f695555e0557110ada127786af0a126c1c59a
luiza <luizasalesgithub@gmail.com>
2026-05-14 23:15:46 -0300

    adiconando migrations, seeders e models com relações

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'email' => 'sysadmin@localhost.com',
+                'password' => bcrypt('Aa123456'),
+                'id_company' => 0,
+                'role' => 'sys-admin',
+                'active' => true,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'email' => 'admin1@localhost.com',
+                'password' => bcrypt('Aa123456'),
+                'id_company' => 1,
+                'role' => 'client-admin',
+                'active' => true,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'email' => 'admin2@localhost.com',
+                'password' => bcrypt('Aa123456'),
+                'id_company' => 2,
+                'role' => 'client-admin',
+                'active' => true,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+        ];
+        DB::table('users')->insert($users);
+        echo count($users)." users inserted successfully!";
+    }
+}
